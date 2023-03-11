be9bb59ab954a063c1f571f40cf4ef9f2084b4e0
hansika_112197 <hansika.piumali@axiatadigitallabs.com>
2023-03-11 14:27:06 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PopupModalComponent } from './popup-modal/popup-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopupModalComponent
   ],
   imports: [
     BrowserModule
